2aa5c39ac64818a98d5f44b91eb4c8cbfbbabd94
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-06-24 12:36:08 +0530

    final commit

--- CountryApp/app.js
+++ CountryApp/app.js
@@ -1,17 +1,55 @@
-let Container = document.getElementById("container")
 
+document.addEventListener("DOMContentLoaded", () => {
+    countryData();
+});
 
 async function countryData() {
 
     try {
         let res = await fetch("https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries ")
 
-        let Data = await res.json()
-        console.log(Data);
+        let countryData = await res.json()
+        country = countryData.data
+        DisplayCountry(country)
+
     } catch (error) {
         console.log("Something went Wrong");
     }
 
 }
 
-countryData()
+
+function DisplayCountry(country) {
+    let Container = document.getElementById("container")
+
+    Container.innerHTML = "";
+    country.forEach((country) => {
+        const countryCard = document.createElement("div");
+        countryCard.className = "country-card";
+
+        const name = document.createElement("h2");
+        name.innerText = country.country;
+        console.log(name);
+
+        const rank = document.createElement("h6");
+        rank.innerText = country.Rank;
+
+        const population = document.createElement("p");
+        population.innerText = country.population;
+
+        countryCard.append(name, rank, population);
+        Container.append(countryCard);
+    });
+}
+
+//function to sort countries by population
+function sortCountriesByPopulation() {
+    country.sort((a, b) => a.population - b.population);
+    DisplayCountry(country);
+}
+
+//Selecting button
+let sortButton = document.getElementById("sort-population");
+
+//added click event listener on button
+sortButton.addEventListener("click", sortCountriesByPopulation);
---
+++ CountryApp/readme.md
@@ -0,0 +1,18 @@
+# Country_Information_App
+
+This application fetches information about countries and sorts them by population. It is built using modern web technologies.
+
+## Features
+
+- Fetches information about countries from a external API.
+- Sorts countries by population in ascending or descending order.
+- Displays detailed information about each country, such as number, and population.
+- Responsive design
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript (ES6+)
+- Fetch API
+- [Country Information API](https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries)
